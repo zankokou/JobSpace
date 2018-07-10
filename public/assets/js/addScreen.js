13e952124853b7e3b15ffa17c6064f205191e293
Scott Gall <scottarthurgall@gmail.com>
2018-07-09 22:57:23 -0500

    add and update job api routes

--- public/assets/js/addScreen.js
+++ public/assets/js/addScreen.js
@@ -1,23 +1,37 @@
 $(document).ready(function(){
+    let id = Number(location.pathname.split('/')[2]);
+    if (!isNaN(id)) {
+        $.ajax("/api/job/"+id).then(function(res) {
+            for (var key in res[0]) {
+                $(`#${key}`).val(res[0][key]);
+            }
+        });
+    }
 
     $("button").click(function(){
         let newJob = {};
 
-        $("input").each(function() {
+        $(".input").each(function() {
             newJob[$(this).attr('name')] = $(this).val().trim();
         });
 
-        newJob["stage"] = $("#stage").val().trim();
-
+        let url = "/api/job";
+        let type = "POST";
+        if (!isNaN(id)) {
+            url += "/"+id
+            type = "PUT"
+        }
         // Send the POST request.
-        $.ajax("/api/job", {
-            type: "POST",
+        $.ajax(url, {
+            type: type,
             data: newJob
         }).then(
             function() {
-                console.log("created new job");
+                console.log("created/edited new job");
                 // Reload the page to get the updated list
-                location.reload();
+                // location.reload();
+                location.pathname = '/jobs';
+
             }
         );
 
